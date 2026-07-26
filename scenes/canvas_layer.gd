c8e7dd0beeb8ce49fa0c613aad6c102e685d87f2
wesleyryolson <wesleyryolson@gmail.com>
2026-07-26 10:19:16 -0500

    Save local canvas layer work

--- scenes/canvas_layer.gd
+++ scenes/canvas_layer.gd
@@ -1,14 +1,22 @@
 extends CanvasLayer
 @onready var color_rect = %ColorRect
 @onready var monkey_hand = %MonkeyHand
+@onready var draw_button = %DrawButton
 
-func _ready() -> void:
-	pass # Replace with function body.
+var _original_zoom: Vector2 = Vector2(1, 1)
+
+func delayed_trigger_camera_action():
+	
+	# Pauses this function execution for 2.0 seconds
+	await get_tree().create_timer(0.6).timeout
+	
+	trigger_camera_action()
 	
 func trigger_camera_action():
 	monkey_hand.modulate.a = 0.0
 	var camera = get_tree().get_first_node_in_group("main_camera")
 	if camera:
+		_original_zoom = camera.zoom # store the zoom level this level started with
 		camera.zoom_to(Vector2(3,3),1.5) 
 	var tween = create_tween()
 	var target_pos = Vector2(offset.x, 0.0) # Replace with your target Y position
@@ -31,9 +39,14 @@ func done():
 	 	.set_ease(Tween.EASE_OUT)
 	var camera = get_tree().get_first_node_in_group("main_camera")
 	if camera:
-		camera.zoom_to(Vector2(1,1),1.5) 
+		camera.zoom_to(_original_zoom, 1.5) 
 	var tween2 = create_tween()
 	tween2.tween_property(monkey_hand, "modulate:a", 0, 0.4)
+	var wheels: Array = []
+	wheels = get_tree().get_nodes_in_group("wheels")
+
+	if draw_button.disabled:
+		draw_button.disabled = false
 
 func clear():
 	color_rect.clear_lines()
